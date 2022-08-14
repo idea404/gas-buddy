3c1c43ce4daa0c7b473cbb4acfdc19a7991c949d
Dennis <10233439+idea404@users.noreply.github.com>
2022-08-14 11:31:39 +0200

    implement winston

--- src/logger.js
+++ src/logger.js
@@ -1,1 +1,24 @@
-// TODO: implement winston logging
+const winston = require('winston');
+
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level}]: ${message}`;
+});
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        myFormat
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});

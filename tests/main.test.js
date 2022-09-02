d4369f3e9eae5ad3dac57a6ffb265f2a111e03f4
Dennis <10233439+idea404@users.noreply.github.com>
2022-09-02 13:19:04 +0000

    remove redundant comment TODOs

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,10 +1,3 @@
-// TODO: include tests for the following:
-// - contract that is >50KB
-// - contract that is not initialized
-// - contract that is not deployed
-// - wrong args provided
-// - function name does not exist
-// - function performs cross-contract calls
 const api = require("../src/api");
 
 describe("gasBuddy", () => {
